f11c46fc044e6d8a862d7612b055c60669eb42d3
Zen T <ztang230@gmail.com>
2024-12-22 16:58:35 -0600

    Initial Commit

---
+++ chatapp/chatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatappApp.swift
+//  chatapp
+//
+//  Created by Zen on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct chatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chatappTests/chatappTests.swift
@@ -0,0 +1,17 @@
+//
+//  chatappTests.swift
+//  chatappTests
+//
+//  Created by Zen on 12/22/24.
+//
+
+import Testing
+@testable import chatapp
+
+struct chatappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
